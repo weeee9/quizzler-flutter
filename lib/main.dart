64f770a6bf3d3c3cd6c31d15e2aa580ed03fa8a4
weeee9 <teletubby332@gmail.com>
2022-11-19 14:53:40 +0800

    fix code

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  void checkAnswer(bool userPickedAnswer) {
     if (quizBrain.isFinished()) {
       Alert(
         context: context,
@@ -64,8 +65,6 @@ class _QuizPageState extends State<QuizPage> {
 
       return;
     }
-
-  void checkAnswer(bool userPickedAnswer) {
 
     bool correctAnswer = quizBrain.getAnswer();
 
